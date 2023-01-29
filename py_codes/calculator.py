0cb7d508f0e1937c2c014734c1da061f65b5e4f0
Rey Cenen <reycenen.pintor@zdspgc.edu.ph>
2023-01-30 00:53:19 +0800

    Done

---
+++ py_codes/calculator.py
@@ -0,0 +1,9 @@
+print('Simple Calculator that adds 2 integers')
+int1 = input('Enter a number :')
+int2 = input('Enter another number :')
+
+result = int(int1+int2)
+
+print(f'Answer {result} ')
+
+
